edf13fe346a0ca26f48d731b45ab4d552a589660
Arnoldisprobablycrazy <97958604+Arnoldisprobablycrazy@users.noreply.github.com>
2022-03-31 16:28:22 +0300

    Update main.c

--- main.c
+++ main.c
@@ -12,22 +12,23 @@
 
 struct Patron {
     char name[100];
+    char pass[30];
     char email[50];
-    char password[30];
     int is_staff;
 };
 
-// Function declarations
+// function prototypes
+int save_patron(struct Patron patron);
 void add_patron();
 int menu();
 void execute_action(int action);
-void save_patron(struct Patron);
+void view_patrons();
 
 int main()
 {
     while(1) {
-        printf("COUNTY LIBRARY SYSTEM\n");
-        printf("Welcome Mr. ARNOLD\n");
+        printf("COUNTY LIBRARY SYSTEM!\n");
+        printf("Welcome Mr. Titus!\n");
         execute_action(menu());
         printf("Press any key to continue");
         getch();
@@ -36,33 +37,45 @@ int main()
     return 0;
 }
 
-void add_patron() {
-    struct Patron patron;
-    printf("Enter name: ");
-    getchar();
-    gets(patron.name);
-    printf("Enter Email: ");
-    gets(patron.email);
-    printf("Enter initial password: ");
-    gets(patron.email);
-    printf("Enter 1 if staff 0 otherwise");
-    scanf("%d",&patron.is_staff);
-    printf("%s added\n",patron.name);
+int save_patron(struct Patron patron){
+    FILE *fp;
+    fp = fopen("patrons","ab");
+    fwrite(&patron,sizeof(struct Patron),1,fp);
+    fclose(fp);
+    return 1;
 }
 
-int menu() {
-    int action;
-    printf("Select an action below\n");
-    printf("1. Add new Patron\n");
-    printf("2. View all patrons\n");
-    printf("3. View all Books\n");
-    printf("4. Add new Book\n");
-    printf("Your action: ");
-    scanf("%d",&action);
-    if(action < 1 || action > 4) {
-        printf("Invalid Action. Try again\n");
-    }
-    return action;
+void add_patron() {
+    struct Patron patron;
+    printf("Enter name:");
+    getchar();
+    gets(patron.name);
+    printf("Enter Email:");
+    gets(patron.email);
+    printf("Initial Password: ");
+    gets(patron.pass);
+    printf("Enter 1 if staff 0 otherwise: ");
+    scanf("%d",&patron.is_staff);
+    if(save_patron(patron))
+        printf("Patron %s successfully added\n",patron.name);
+    else
+        printf("Unsuccessful\n");
+}
+
+int menu()
+{
+  int action;
+  printf("Select an action:\n");
+  printf("1. Add new patron\n");
+  printf("2. View Patrons\n");
+  printf("3. View Books\n");
+  printf("4. Add New Book\n");
+  printf("Your Action: ");
+  scanf("%d",&action);
+  if(action < 1 || action > 4) {
+    printf("Invalid action. Try again\n");
+  }
+  return action;
 }
 
 void execute_action(int action) {
@@ -71,15 +84,35 @@ void execute_action(int action) {
         add_patron();
         break;
     case 2:
-        printf("list of all patrons\n");
+        view_patrons();
         break;
     case 3:
-        printf("list of all books\n");
+        printf("Here is a list of all books");
         break;
     case 4:
-        printf("adding a new book\n");
+        printf("adding a new Book\n");
         break;
-    default: printf("Invalid action.\n");
+    default:
+        printf("Invalid action.\n");
     }
 }
+
+void view_patrons() {
+    FILE *fp;
+    struct Patron patron;
+    if((fp = fopen("patrons","rb")) == NULL) {
+        printf("Unable to open file.\n");
+        return;
+    }
+    printf("%-20s%-30s%-10s\n","NAME","EMAIL","IS STAFF");
+    while(!feof(fp)){
+       fread(&patron,sizeof(struct Patron),1,fp);
+       printf("%-20s",patron.name);
+       printf("%-30s",patron.email);
+       printf("%-10d\n",patron.is_staff);
+    }
+
+    fclose(fp);
+}
+
 
